58617e4dfef92ecace688f496eb15347a39ac6c4
Evair Macollunco <mevairmc@gmail.com>
2022-10-25 10:58:14 -0500

    test: calculate the product tax

--- main_spec.rb
+++ main_spec.rb
@@ -1,29 +1,36 @@
 require './main'
 
 RSpec.describe CashRegister do
-  describe '#initialize' do
-    it 'convert information str in hash information' do
-      cashRegister = CashRegister.new([
-        "1 imported box of chocolates at 10.00",
-        "1 imported bottle of perfume at 47.50"
-      ])
 
-      expect(cashRegister.products).to eq([
-        {
-          quantity: 1,
-          imported: true,
-          exception: true,
-          name: 'imported box of chocolates',
-          value: 10.00
-        },
-        {
-          quantity: 1,
-          imported: true,
-          exception: false,
-          name: 'imported bottle of perfume',
-          value:47.50
-        }
-      ])
-    end
+  before(:each) do 
+    @cash_register = CashRegister.new([
+      "1 imported box of chocolates at 10.00",
+      "1 imported bottle of perfume at 47.50"
+    ])
+  end 
+
+  it 'convert information str in hash information' do
+
+    expect(@cash_register.products).to eq([
+      {
+        quantity: 1,
+        imported: true,
+        exception: true,
+        name: 'imported box of chocolates',
+        value: 10.00
+      },
+      {
+        quantity: 1,
+        imported: true,
+        exception: false,
+        name: 'imported bottle of perfume',
+        value: 47.50
+      }
+    ])
+  end
+
+  it 'Calculate a product tax' do
+    expect(@cash_register.send(:calculate_taxes, @cash_register.products[0])).to eq(0.5)
+    expect(@cash_register.send(:calculate_taxes, @cash_register.products[1])).to eq(7.15)
   end
 end
